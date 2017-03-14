ecbc92133e6c2f2e7102a20ab88f3a62d62d27fe
Luke Rickard <luke.x.rickard@gmail.com>
2017-03-14 14:10:58 +0000

    removing testing pays api key

--- worldpay_python_example_app/settings/dev.py
+++ worldpay_python_example_app/settings/dev.py
@@ -4,5 +4,5 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
 
-WORLDPAY_SERVICE_KEY = os.environ.get("WORLDPAY_SERVICE_KEY", 'e0e1db6fa2d205b0a755e2284c0d87fa')
+WORLDPAY_SERVICE_KEY = os.environ.get("WORLDPAY_SERVICE_KEY", '<Insert TestingPays API key here>')
 WORLDPAY_ORDERS_URL = os.environ.get("WORLDPAY_ORDERS_URL", "http://localhost:8000/{0}/worldpay/v1/orders".format(WORLDPAY_SERVICE_KEY))
