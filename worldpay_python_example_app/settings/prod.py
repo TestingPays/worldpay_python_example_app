31e9fe763b390c98688a3c9a4b51b47035f43325
Luke Rickard <luke.x.rickard@gmail.com>
2017-03-14 14:14:15 +0000

    removing testing pays api key from prod settings file

--- worldpay_python_example_app/settings/prod.py
+++ worldpay_python_example_app/settings/prod.py
@@ -1,4 +1,4 @@
 from worldpay_python_example_app.settings.base import *
 
-WORLDPAY_SERVICE_KEY = os.environ.get("WORLDPAY_SERVICE_KEY", 'e0e1db6fa2d205b0a755e2284c0d87fa')
+WORLDPAY_SERVICE_KEY = os.environ.get("WORLDPAY_SERVICE_KEY", '')
 WORLDPAY_ORDERS_URL = os.environ.get("WORLDPAY_ORDERS_URL", "https://api.worldpay.com/v1/orders")
